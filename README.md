2a4a74a2144d7bf8ed64c667b320482b873c7b4e
FAXES <31368070+FAXES@users.noreply.github.com>
2021-01-24 12:36:49 +1100

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,145 @@
-# syntax-checker-new
+# Code Syntax Checker (New)
+---
+
 This package is aimed at providing a basic syntax check for many languages (listed below) to ensure correct syntax.
+
+You do need to install a few things for each one to work on your system. Click on the languages to scroll to point.
+
+Check syntax via string or file!
+
+
+### Supported Languages
+- [PHP](#php)
+- [Ruby](#ruby)
+- [Python](#python)
+- [Perl](#perl)
+- [Lua (needs fixing)](#lua)
+- [Bash (needs fixing)](#bash1)
+- [c](#c-and-c++)
+- [c++](#c-and-c++)
+- [js](#javascript)
+
+---
+
+## Installing
+
+To install this package use the below command. Reference each language below for those steps.
+```
+npm i syntax-checker-new
+```
+
+## Examples & Usage for `checkSyntaxString()`
+- `codeString`: The string of code you wish to validate.
+- `language`: The language your string of code is. This is the language file extension. For example C++ would be `cpp`.
+- `callback`: The functions call back object.
+    - `passed`: Boolean of whether or not the code passed.
+    - `error`: The error code for the syntax error if passed = false. **This is currently not used.**
+
+
+```js
+checkSyntaxString(codeString, language, function(syntaxReturn){
+    console.log(syntaxReturn.passed);
+    console.log(syntaxReturn.error);
+});
+```
+
+Javascript Example.
+```js
+let jscode = `console.log(syntaxReturn);`;
+checkSyntaxString(jscode, "js", function(syntaxReturn){
+    console.log(syntaxReturn.passed) // Expected result: true
+});
+```
+
+PHP Example.
+```js
+let phpcode = `<?php echo 'test message; ?>`;
+checkSyntaxString(phpcode, "php", function(syntaxReturn){
+    console.log(syntaxReturn.passed) // Expected result: false (as the quote is not closed)
+});
+```
+
+---
+
+## Examples & Usage for `checkSyntaxFile()`
+- `filePath`: The file path of the file to check.
+- `language`: The language your string of code is. This is the language file extension. For example C++ would be `cpp`.
+- `callback`: The functions call back object.
+    - `passed`: Boolean of whether or not the code passed.
+    - `error`: The error code for the syntax error if passed = false. **This is currently not used.**
+
+
+```js
+checkSyntaxFile(filePath, language, function(syntaxReturn){
+    console.log(syntaxReturn.passed);
+    console.log(syntaxReturn.error);
+});
+```
+
+Javascript Example.
+```js
+let jspath = `/root/test.js`;
+checkSyntaxFile(jspath, "js", function(syntaxReturn){
+    console.log(syntaxReturn.passed)
+});
+```
+
+PHP Example.
+```js
+let phppath = `/root/test.php`;
+checkSyntaxFile(phppath, "php", function(syntaxReturn){
+    console.log(syntaxReturn.passed)
+});
+```
+
+---
+
+## Specific Languages
+
+Each language you wish to syntax check may require that language to be installed on your system.
+
+### PHP
+If you intend to use PHP syntax checking you will need to install PHP on your system. See the PHP website below for instructions.
+
+https://www.php.net/manual/en/install.php
+
+### Ruby
+To use ruby in syntax checking you need to install Ruby. See the below site for more.
+
+https://www.ruby-lang.org/en/documentation/installation/
+
+### Python
+To syntax check Python scripts install Python
+
+https://www.python.org/downloads/
+
+### Perl
+If you want to syntax check Perl, install Perl.
+
+https://www.perl.org/get.html
+
+### Lua
+If you intend to syntax check for Lua errors you need to have Lua installed
+
+http://www.lua.org/download.html
+
+### Bash
+Install Bash to syntax check bash scripts.
+
+https://www.gnu.org/software/bash/
+
+### C And C++
+These use the same program as the languages are very alike. See the below link.
+
+https://gcc.gnu.org/install/
+
+### Javascript
+You're in luck! Javascript uses node for syntax checking so if you have Node.Js installed (which you likely do) then you're fine!
+
+
+---
+
+## Support
+If you need support create a comment on GitHub or if you use Discord talk to the creator (FAXES & Beefer)
+
+[![Discord](https://faxes.zone/i/9wkr3.png)](https://faxes.zone/discord)
---
+++ index.js
@@ -0,0 +1,211 @@
+var fs = require('fs');
+os = require('os');
+exec = require('child_process').exec;
+
+function makeid(length) {
+    if(!length) length = 40;
+    var result           = '';
+    var characters       = '0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+async function checkSyntaxString(string, language, callback) {
+    fileId = makeid();
+    string = string || ``;
+    language = language || '.php'
+    fs.writeFile(`./files/${fileId}.${language}`, string, 'utf8', (err) => {
+        if (err) console.log(err);
+    });
+
+    if(language == 'php') {
+        exec(`php -l ${__dirname}/files/${fileId}.php`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'py') {
+        exec(`python3 -m py_compile ${__dirname}/files/${fileId}.py`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'rb') {
+        exec(`ruby -c ${__dirname}/files/${fileId}.rb`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'pl') {
+        exec(`perl -c ${__dirname}/files/${fileId}.pl`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'lua') { // BROKEN
+        exec(`luac -p ${__dirname}/files/${fileId}.lua`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'sh') { // BROKEN
+        exec(`bash -n ${__dirname}/files/${fileId}.sh`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'c') {
+        exec(`gcc -fsyntax-only -Werror ${__dirname}/files/${fileId}.c`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'cpp') {
+        exec(`gcc -fsyntax-only -Werror ${__dirname}/files/${fileId}.cpp`, function(err, stdout, stderr){
+            if(err){
+                console.log(err)
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'js') {
+        exec(`node --check ${__dirname}/files/${fileId}.js`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+}
+
+
+async function checkSyntaxFile(filePath, language, callback) {
+    language = language || '.php'
+
+    if(language == 'php') {
+        exec(`php -l ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'py') {
+        exec(`python3 -m py_compile ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'rb') {
+        exec(`ruby -c ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'pl') {
+        exec(`perl -c ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'lua') { // BROKEN
+        exec(`luac -p ${filePath}a`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'sh') { // BROKEN
+        exec(`bash -n ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'c') {
+        exec(`gcc -fsyntax-only -Werror ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'cpp') {
+        exec(`gcc -fsyntax-only -Werror ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                console.log(err)
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+
+    if(language == 'js') {
+        exec(`node --check ${filePath}`, function(err, stdout, stderr){
+            if(err){
+                callback({passed: false, error: null});
+                return;
+            }
+            callback({passed: true, error: null});
+        });
+    }
+}
